5fd26d1544e7e29ac83977e4a17b0b389d6fa166
LiangZijun <849140151@qq.com>
2024-08-19 17:02:52 +0800

    refactor(Muse.UI *****DI******:): trying to use dependency injection！

--- Muse.UI/App.xaml.cs
+++ Muse.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Win = System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Win = System.Windows;
 
 
 namespace Muse.UI;
@@ -8,4 +10,32 @@ namespace Muse.UI;
 /// </summary>
 public partial class App : Win.Application
 {
+    public static IHost? AppHost { get; private set; }
+
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+           .ConfigureServices((hostContext, services) =>
+           {
+               services.AddSingleton<MainWindow>();
+
+           })
+           .Build();
+    }
+
+    protected override async void OnStartup(Win.StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+        var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+        startupForm.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(Win.ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit( e);
+    }
+
 }
