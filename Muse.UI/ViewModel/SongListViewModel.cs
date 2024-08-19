9a5e51d1ced92f62773951caa2b8e6f7f3d6beab
LiangZijun <849140151@qq.com>
2024-08-19 13:25:23 +0800

    feat(Muse.UI): SongListViewModel: add save and delete function
    1. add the SaveSong method still need to distinct from database
    2. add a template DeleteSong method

--- Muse.UI/ViewModel/SongListViewModel.cs
+++ Muse.UI/ViewModel/SongListViewModel.cs
@@ -13,6 +13,17 @@ public class SongListViewModel : ViewModelBase
 {
     public ObservableCollection<SongBasic> SongBasic { get; set; }
 
+    public SongBasic selectSong;
+    public SongBasic SelectSong
+    {
+        get => selectSong;
+        set
+        {
+            selectSong = value;
+            OnPropertyChanged();
+        }
+    }
+
     public SongListViewModel()
     {
         // Select all songs from database to initialize the songlist
@@ -23,6 +34,7 @@ public class SongListViewModel : ViewModelBase
         }
     }
 
+    #region Selct a folder and get the songs in it
     public RelayCommand SelectFolderCommand => new RelayCommand(execute => SelectFolder());
     private void SelectFolder()
     {
@@ -45,32 +57,78 @@ public class SongListViewModel : ViewModelBase
 
             foreach (SongBasic? newSong in newSongs)
             {
-                    SongBasic.Add(newSong ?? throw new InvalidOperationException());
+                SongBasic.Add(newSong ?? throw new InvalidOperationException());
             }
         }
     }
 
 
+    #endregion
+
+    #region Save songs to database
+
+    public RelayCommand SaveSongCommand => new RelayCommand(execute => SaveSong(), canExecute => CanSave());
+
+    private void SaveSong()
+    {
+        // saving the SongBasic to database
+        using (MyDbContext db = new MyDbContext())
+        {
+            // 获取数据库中已有的歌曲
+            var existingSongs = db.SongBasic.ToList();
+            var existingSongIds = new HashSet<int>(existingSongs.Select(s => s.SongBasicId));
+
+            // 过滤掉已存在的歌曲
+            var newSongs = SongBasic
+                .Where(song => !existingSongIds.Contains(song.SongBasicId))
+                .ToList();
+
+            db.SongBasic.AddRange(newSongs);
+            db.SaveChanges();
+            // Todo: need to check the song if it exists in the database, if not, add it.
+        }
+    }
+
+    private bool CanSave()
+    {
+        return true;
+    }
+
+    #endregion
+
+    #region Delete songs to database
+
+    public RelayCommand DeleteSongCommand => new RelayCommand(execute => DeleteSong(), canExecute => SelectSong != null);
+
+    private void DeleteSong()
+    {
+        // saving the SongBasic to database
+        using (MyDbContext db = new MyDbContext())
+        {
+            // SongBasic.Remove(SelectSong);
+            // Find that sqlite can not delete cascade, but EF core can.
+            var tukiSongs = db.SongBasic.First(s => s.Performers == "tuki.");
+            db.Remove(tukiSongs);
+            db.SaveChanges();
+
+        }
+    }
+
+    #endregion
+
+
+    #region Demo function for using RelayCommand
+
     public RelayCommand PrintSongCommand => new RelayCommand(execute => PrintSong(), canExecute=> SelectSong != null);
-
-
     private void PrintSong()
     {
         Console.WriteLine(SelectSong.Title);
         SongDetail detail = new SongDetail();
     }
 
-    public SongBasic selectSong;
+    #endregion
 
-    public SongBasic SelectSong
-    {
-        get => selectSong;
-        set
-        {
-            selectSong = value;
-            OnPropertyChanged();
-        }
-    }
+
 
 
 
