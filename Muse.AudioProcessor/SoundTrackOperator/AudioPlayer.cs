c84ee0ac3cd0e6fa395b44abae22cb5f37125f78
LiangZijun <849140151@qq.com>
2024-08-21 07:30:11 +0800

    feat(Muse.AudioProcessor): add a new class to play audio

---
+++ Muse.AudioProcessor/SoundTrackOperator/AudioPlayer.cs
@@ -0,0 +1,54 @@
+﻿using NAudio.Wave;
+
+public static class AudioPlayer
+{
+    private static IWavePlayer waveOut;
+    private static AudioFileReader audioFile;
+
+    public static void Load(string filePath)
+    {
+        Dispose(); // 释放之前的资源
+        waveOut = new WaveOutEvent();
+        audioFile = new AudioFileReader(filePath);
+        waveOut.Init(audioFile);
+    }
+
+    public static void Play()
+    {
+        waveOut.Play();
+        // while (waveOut.PlaybackState == PlaybackState.Playing)
+        // {
+        //     Thread.Sleep(100);
+        // }
+    }
+
+    public static void Pause()
+    {
+        waveOut.Pause();
+    }
+
+    public static void Stop()
+    {
+        waveOut.Stop();
+        if (audioFile != null) audioFile.Position = 0;
+    }
+
+    public static void SetPosition(TimeSpan position)
+    {
+        if (audioFile != null && position < audioFile.TotalTime) audioFile.CurrentTime = position;
+    }
+
+    public static void SetVolume(float volume)
+    {
+        if (audioFile != null) audioFile.Volume = volume; // 0.0 to 1.0
+    }
+
+
+    public static void Dispose()
+    {
+        waveOut?.Dispose();
+        audioFile?.Dispose();
+        waveOut = null;
+        audioFile = null;
+    }
+}
--- Muse.ConsoleControl/Program.cs
+++ Muse.ConsoleControl/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Muse.AudioProcessor.SoundTrackOperator;
 using Muse.DB.Configuration;
 
 namespace Muse.ConsoleControl;
@@ -8,21 +9,52 @@ public class Program
 {
     static void Main()
     {
-        IServiceProvider serviceProvider = ConfigureServices();
+        AudioPlayer.Load(@"D:\Project\Muse\Muse.Resources\Music\Songs\tuki. - 晩餐歌.mp3");
 
-        using (var scope = serviceProvider.CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+        Console.WriteLine("Playing audio");
+        AudioPlayer.Play();
+        Thread.Sleep(5000); // 播放5秒
 
-            // Example database operation
-            var songs = dbContext.SongBasic.ToList();
-            foreach (var song in songs)
-            {
-                Console.WriteLine($"Song: {song.Title}");
-            }
-        }
+        Console.WriteLine("lowering volume of audio");
+        AudioPlayer.SetVolume(0.5f); // 设置音量为50%
 
+        Console.WriteLine("Pausing audio");
+        AudioPlayer.Pause();
+        System.Threading.Thread.Sleep(2000); // 暂停2秒
+        //
+        Console.WriteLine("Resuming audio");
+        AudioPlayer.Play();
+        Thread.Sleep(3000); // 播放3秒
+
+        Console.WriteLine("Jumping to 10 seconds");
+        AudioPlayer.SetPosition(TimeSpan.FromSeconds(10));
+        AudioPlayer.Play();
+        System.Threading.Thread.Sleep(5000); // 播放5秒
+
+        Console.WriteLine("Stopping audio");
+        AudioPlayer.Stop();
+
+        AudioPlayer.Dispose();
     }
+
+
+    // static void Main()
+    // {
+    //     IServiceProvider serviceProvider = ConfigureServices();
+    //
+    //     using (var scope = serviceProvider.CreateScope())
+    //     {
+    //         var dbContext = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+    //
+    //         // Example database operation
+    //         var songs = dbContext.SongBasic.ToList();
+    //         foreach (var song in songs)
+    //         {
+    //             Console.WriteLine($"Song: {song.Title}");
+    //         }
+    //     }
+    //
+    // }
 
     private static IServiceProvider ConfigureServices()
     {
