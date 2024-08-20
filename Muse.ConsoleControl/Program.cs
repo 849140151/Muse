2c97c629e86830f883bbbbc785cdaeda0d773494
LiangZijun <849140151@qq.com>
2024-08-20 15:09:13 +0800

    refactor(Muse.ConsoleControl): using DI for database in ConsoleControl

--- Muse.ConsoleControl/Program.cs
+++ Muse.ConsoleControl/Program.cs
@@ -1,14 +1,37 @@
-﻿using Muse.DB.Configuration;
-using Muse.DB.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Muse.DB.Configuration;
 
 namespace Muse.ConsoleControl;
 
-class Program
+public class Program
 {
     static void Main()
     {
-        using ( MyDbContext context = new MyDbContext())
+        IServiceProvider serviceProvider = ConfigureServices();
+
+        using (var scope = serviceProvider.CreateScope())
         {
+            var dbContext = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+
+            // Example database operation
+            var songs = dbContext.SongBasic.ToList();
+            foreach (var song in songs)
+            {
+                Console.WriteLine($"Song: {song.Title}");
+            }
         }
+
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+        string slnFolder = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\.."));
+        string dbPath = Path.Combine(slnFolder, "Muse.DB", "Muse.sqlite");
+        services.AddDbContext<MyDbContext>(options => options.UseSqlite($"Data Source={dbPath}"));
+
+        return services.BuildServiceProvider();
+
     }
 }
