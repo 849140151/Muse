26ec9cb7f003bf5be50a57d3b92670ad4f8be3bc
LiangZijun <849140151@qq.com>
2024-08-22 09:43:15 +0800

    feat(Muse.UI): Add whole bunch of styles in the project

---
+++ Muse.UI/Utilities/Btn.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using Controls = System.Windows.Controls;
+
+namespace Muse.UI.Utilities;
+
+public class Btn : Controls.RadioButton
+{
+    static Btn()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(Btn), new FrameworkPropertyMetadata(typeof(Btn)));
+    }
+}
