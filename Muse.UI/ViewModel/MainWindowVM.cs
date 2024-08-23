65baff12abae78ccae0e1b9eada37cbcf31e10e2
LiangZijun <849140151@qq.com>
2024-08-23 09:00:46 +0800

    perf(Muse.UI): MainWindow actually using the Navigation DataContext, no longer need to inject into MainWindow itself

--- Muse.UI/ViewModel/MainWindowVM.cs
+++ Muse.UI/ViewModel/MainWindowVM.cs
@@ -1,24 +1,8 @@
-﻿namespace Muse.UI.ViewModel;
+﻿using Muse.UI.Utilities;
 
-public class MainWindowVM
+namespace Muse.UI.ViewModel;
+
+public class MainWindowVM : ViewModelBase
 {
-    private NavigationVM NavigationVm { get; set; }
-    public PlayBarVM PlayBarVm { get; set; }
-    public SongListVM SongListVm { get; set; }
-    public HomeVM HomeVm { get; set; }
-    public LyricVM LyricVm { get; set; }
 
-    public MainWindowVM(
-        SongListVM songListVm, PlayBarVM playBarVm,
-        NavigationVM navigationVm, HomeVM homeVm,
-        LyricVM lyricVm
-        )
-    {
-        NavigationVm = navigationVm;
-        PlayBarVm = playBarVm;
-        SongListVm = songListVm;
-        HomeVm = homeVm;
-        LyricVm = lyricVm;
-
-    }
 }
